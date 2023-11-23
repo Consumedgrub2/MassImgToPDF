0bcba8c156e37850f0d31552e8e8973955a54cde
Consumedgrub2 <maherscp0@gmail.com>
2023-11-23 01:14:19 -0700

    Rename file structure for csharp conventions. Also mention fix for #10 and #8

--- main/Properties/Resources.Designer.cs
+++ main/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MassImgToPDF.main.Properties {
+namespace MassImgToPDF.Main.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MassImgToPDF.main.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MassImgToPDF.main.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MassImgToPDF.Main.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
